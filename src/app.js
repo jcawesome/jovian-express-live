9590cac8c07f0b8bbbd62cb1a456c22bc75c3000
jcawesome <pagtalunanjancarlo@hotmail.com>
2023-04-18 14:38:51 +0000

    converted index.html to index.mustache

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const path = require('path');
+const mustacheExpress = require('mustache-express');
+const JOBS = require('./jobs');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')))
+// Configure mustache
+app.set('views', `${__dirname}/pages`);
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
 
+// Render the template
 app.get('/', (req, res) => {
-    // res.send('Hello World!');
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
-});
+    res.render('index', { jobs: JOBS});
+})
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server running on https://localhost:${port}`);
-});
+    console.log(`Server running on https://localhost:${port}`)
+})
